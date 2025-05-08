b499a53e5449dbc9863d8f5377836b05792ea5e0
Andis Paudel <andispaudel@gmail.com>
2025-05-08 12:23:11 +0545

    socket headers added

---
+++ headers/sockets_headers.h
@@ -0,0 +1,19 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <errno.h>
+#include <fcntl.h>
+
+#define ISVALIDSOCKET(s) ((s) >= 0)
+#define CLOSESOCKET(s) close(s)
+#define SOCKET int
+#define GETSOCKETERRNO() (errno)
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
